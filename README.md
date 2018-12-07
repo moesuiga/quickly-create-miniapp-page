1e49136e0ee99a253d84832e7fdda31ad7ad3416
moesuiga <wyw1121@live.com>
2018-12-08 01:45:48 +0800

    feat: Release v0.0.1
    Quickly create template pages for miniapp

---
+++ README.md
@@ -0,0 +1,35 @@
+# quickly-create-miniapp-page
+
+快速创建小程序页面
+
+## Install
+
+``` bash
+$ npm i -g quickly-create-miniapp-page
+```
+
+## Usage
+
+### Default create page for typescript
+
+``` bash
+$ cmp pages/home/home # create four files for page/home/home.{ts, json, wxml, wxss}
+```
+
+### Create page for javascript
+
+``` bash
+$ cmp pages/java/script -s js # -s abbreviate from --script
+```
+
+### Create multiple pages
+
+``` bash
+$ cmp pages/first/page pages/another/page
+```
+
+### For my colleagues
+
+``` bash
+$ cmp --th pages/suit/page
+```
---
+++ bin/cmp.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const program = require('commander');
+const colors = require('colors');
+const create = require('../lib/create');
+
+const root = process.cwd();
+
+program
+  .version('0.0.1')
+  .description(`
+  Quickly Create pages for miniapp.
+
+  ${'e.g.'.blue}
+
+    ${'$'.gray} ${'cmp index/index'.green}
+
+  Out:
+
+    - ${'index/index.json'.magenta}
+    - ${'index/index.js'.magenta}
+    - ${'index/index.wxml'.magenta}
+    - ${'index/index.wxss'.magenta}
+  `);
+
+program
+  .option('-s, --script [type]', 'The type of script file. [js/ts]', 'ts')
+  .option('-p, --page [type]', 'page/P or component/C', 'page')
+  .option('--th', 'Use with my company')
+  .parse(process.argv);
+
+if (!program.args || program.args.length === 0) {
+  program.help();
+} else {
+  create(program);
+}
